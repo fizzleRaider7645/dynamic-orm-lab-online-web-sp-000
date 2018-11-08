c4ae12b8c9914a4aa2be50da26294ef798b85e09
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-08 15:00:11 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -56,7 +56,8 @@ class InteractiveRecord
   
   def self.find_by(attribute)
     sql = <<-SQL 
-      SELECT * FROM #{self.table_name} WHERE #{attribute.keys.first.to_s} = '#{attribute.values.first.to_s}'
+      SELECT * 
+      FROM #{self.table_name} WHERE #{attribute.keys.first.to_s} = '#{attribute.values.first.to_s}'
     SQL
     DB[:conn].execute(sql)
   end
